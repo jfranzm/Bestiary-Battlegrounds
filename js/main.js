a3f92a565dc19311ac46afbfdd820a5386d3c21e
Jeffrey Martinez <martinez.jeffreyfranz@gmail.com>
2022-12-07 21:33:44 -0500

    update battle UI + turn counter

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 let playerUnit, enemyUnit, background, HP;
+let turn = 1
+function turnCounter() {
+    turn++;
+}
+console.log(turn);
+let playerChoice = '';
 
+let choice = document.getElementsByClassName('playerInput');
+for (idx=0; idx < playerChoice.length; idx++) {
+    playerChoice[idx].addEventListener('click', playerChoice);
+}
+
+// function playerTurn() {
+//     if(turn % 2 !== 0){
+
+//     }
+// }
+// function enemyTurn() {
+//     if(turn ===)
+// } 
