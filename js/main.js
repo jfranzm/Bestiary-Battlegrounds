d16aa7c17d03948aae8ca5aa1a4783067ec3d1e2
Jeffrey Martinez <martinez.jeffreyfranz@gmail.com>
2022-12-08 17:12:35 -0500

    create playerUnit + enemyUnit

--- js/main.js
+++ js/main.js
@@ -1,25 +1,56 @@
-let playerUnit, enemyUnit, background, HP;
+/*---- state variables -----*/
+let playerUnit, enemyUnit;
 // Unit class
 class Unit {
-    constructor(HP, atkStat, options) {
+    constructor(totalHP, HP, atkStat, options) {
+        this.totalHP = totalHP;
         this.HP = HP;
         this.atkStat = atkStat;
         this.options = options;
     }
+    dmgCalc(target) {
+        target.HP - (Math.floor(Math.random()* this.atkStat))
+        render();
+    }
 }
 
+// playerUnit stats
+playerUnit = new Unit(30, 30, 10, {
+    // player options
+    attack: dmgCalc(target)
+})
+
+// enemyUnit stats
+enemyUnit = new Unit(40, 40, 6, {
+    // enemy options
+    attack: dmgCalc(target)
+})
+// turn counter
 let turn = 1
+
+/*----- functions -----*/
 function turnCounter() {
     turn++;
 }
-// let playerChoice = '';
 
-// function playerInput(){
-//     let choice = document.getElementsByClassName('playerInput');
-//     for (idx=0; idx < playerChoice.length; idx++) {
-//         playerChoice[idx].addEventListener('click', playerChoice);
+// win condition
+// function playerWin() {
+//     if (playerUnit.HP <= 0) {
+
 //     }
 // }
+/*----- event listners -----*/
+function playerInput(){
+    let choice = document.getElementsByClassName('playerInput');
+    for (idx=0; idx < playerChoice.length; idx++) {
+        playerChoice[idx].addEventListener('click', playerChoice);
+    }
+}
+
+
+// let playerChoice = '';
+
+
 
 // function playerTurn() {
 //     if(turn % 2 !== 0){
