0f837e2f6164829d7756711092188bed8536b906
Jeffrey Martinez <martinez.jeffreyfranz@gmail.com>
2022-12-07 21:44:59 -0500

    create UNIT class

--- js/main.js
+++ js/main.js
@@ -1,15 +1,25 @@
 let playerUnit, enemyUnit, background, HP;
+// Unit class
+class Unit {
+    constructor(HP, atkStat, options) {
+        this.HP = HP;
+        this.atkStat = atkStat;
+        this.options = options;
+    }
+}
+
 let turn = 1
 function turnCounter() {
     turn++;
 }
-console.log(turn);
-let playerChoice = '';
+// let playerChoice = '';
 
-let choice = document.getElementsByClassName('playerInput');
-for (idx=0; idx < playerChoice.length; idx++) {
-    playerChoice[idx].addEventListener('click', playerChoice);
-}
+// function playerInput(){
+//     let choice = document.getElementsByClassName('playerInput');
+//     for (idx=0; idx < playerChoice.length; idx++) {
+//         playerChoice[idx].addEventListener('click', playerChoice);
+//     }
+// }
 
 // function playerTurn() {
 //     if(turn % 2 !== 0){
